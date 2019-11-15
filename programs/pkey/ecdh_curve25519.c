a154f155ebc3af30457ad7ad30365b93dab8dd83
Jakob Buron <jakob.buron@silabs.com>
2019-11-15 15:18:13 +0100

    Demonstrate failure to load curve25519 keys
    Attempt to load private keys from RFC7748 and compute a shared secret.
    Throws MBEDTLS_ERR_ECP_INVALID_KEY.

--- programs/pkey/ecdh_curve25519.c
+++ programs/pkey/ecdh_curve25519.c
@@ -36,6 +36,8 @@
 #define MBEDTLS_EXIT_FAILURE    EXIT_FAILURE
 #endif /* MBEDTLS_PLATFORM_C */
 
+#include <string.h>
+
 #if !defined(MBEDTLS_ECDH_C) || !defined(MBEDTLS_ECDH_LEGACY_CONTEXT) || \
     !defined(MBEDTLS_ECP_DP_CURVE25519_ENABLED) || \
     !defined(MBEDTLS_ENTROPY_C) || !defined(MBEDTLS_CTR_DRBG_C)
@@ -150,6 +152,14 @@ int main( int argc, char *argv[] )
     /*
      * Server: read peer's key and generate shared secret
      */
+     /* These test vectors come from https://cr.yp.to/highspeed/naclcrypto-20090310.pdf and are repeated in RFC7748 */
+     unsigned char alicesk[32] = { 0x77,0x07,0x6d,0x0a,0x73,0x18,0xa5,0x7d ,0x3c,0x16,0xc1,0x72,0x51,0xb2,0x66,0x45 ,0xdf,0x4c,0x2f,0x87,0xeb,0xc0,0x99,0x2a ,0xb1,0x77,0xfb,0xa5,0x1d,0xb9,0x2c,0x2a };
+
+     unsigned char bobpk[32] = { 0xde,0x9e,0xdb,0x7d,0x7b,0x7d,0xc1,0xb4 ,0xd3,0x5b,0x61,0xc2,0xec,0xe4,0x35,0x37 ,0x3f,0x83,0x43,0xc8,0x5b,0x78,0x67,0x4d ,0xad,0xfc,0x7e,0x14,0x6f,0x88,0x2b,0x4f };
+
+     unsigned char shared_bob_alice[32] = { 0x4a,0x5d,0x9d,0x5b,0xa4,0xce,0x2d,0xe1 ,0x72,0x8e,0x3b,0xf4,0x80,0x35,0x0f,0x25 ,0xe0,0x7e,0x21,0xc9,0x47,0xd1,0x9e,0x33 ,0x76,0xf0,0x9b,0x3c,0x1e,0x16,0x17,0x42 };
+
+
     mbedtls_printf( "  . Server reading client key and computing secret..." );
     fflush( stdout );
 
@@ -160,13 +170,17 @@ int main( int argc, char *argv[] )
         goto exit;
     }
 
-    ret = mbedtls_mpi_read_binary( &ctx_srv.Qp.X, cli_to_srv, 32 );
+    ret = mbedtls_mpi_read_binary( &ctx_srv.Qp.X, bobpk, 32 );
     if( ret != 0 )
     {
         mbedtls_printf( " failed\n  ! mbedtls_mpi_read_binary returned %d\n", ret );
         goto exit;
     }
 
+    mbedtls_mpi_read_binary( &ctx_srv.d, alicesk, 32 );
+
+    /* This returns MBEDTLS_ERR_ECP_INVALID_KEY from mbedtls_ecp_check_privkey(),
+      so we are probably messing up when loading ctx_srv.d */
     ret = mbedtls_ecdh_compute_shared( &ctx_srv.grp, &ctx_srv.z,
                                        &ctx_srv.Qp, &ctx_srv.d,
                                        mbedtls_ctr_drbg_random, &ctr_drbg );
@@ -177,6 +191,12 @@ int main( int argc, char *argv[] )
     }
 
     mbedtls_printf( " ok\n" );
+
+    char string_buf[2000];
+    size_t olen;
+    memset(string_buf, 0, sizeof string_buf);
+    mbedtls_mpi_write_string(&ctx_srv.z, 16, string_buf, sizeof string_buf, &olen);
+    mbedtls_printf("shared secret: %s\n", string_buf);
 
     /*
      * Client: read peer's key and generate shared secret
